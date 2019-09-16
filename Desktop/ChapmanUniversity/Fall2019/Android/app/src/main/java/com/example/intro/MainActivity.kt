415930f1e601b3c4322532f1b5fc3356ed696cc2
Jayson Marshall <marsh208@mail.chapman.edu>
2019-09-15 18:12:49 -0700

    first commit

---
+++ Desktop/ChapmanUniversity/Fall2019/Android/app/src/main/java/com/example/intro/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
+import androidx.core.view.isVisible
+
+class MainActivity : AppCompatActivity() {
+    var count = 0;
+
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState?.run {
+            putString("counter", counter.text.toString())
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        counter.text = savedInstanceState?.getString("counter")
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var tempCount = savedInstanceState?.getString("counter");
+        if (tempCount != null) {
+            count = tempCount.toInt()
+        }
+        myButton.setOnClickListener{
+            count = count + 1;
+            if(image.isVisible)
+            {
+                image.visibility = View.GONE
+            }
+            else
+            {
+                image.visibility = View.VISIBLE
+            }
+            counter.text = count.toString();
+        }
+
+
+    }
+}
