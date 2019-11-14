a39e924f15e71cbb5b0fec05e89009233224afae
Jayson Marshall <marsh208@mail.chapman.edu>
2019-11-14 12:56:29 -0800

    implemented giphy api using LiveData and Retrofit

--- Desktop/ChapmanUniversity/Fall2019/Android/AndroidClicker/app/src/main/java/com/example/clickerandroidarchitecture/ui/MainActivity.kt
+++ Desktop/ChapmanUniversity/Fall2019/Android/AndroidClicker/app/src/main/java/com/example/clickerandroidarchitecture/ui/MainActivity.kt
@@ -12,6 +12,9 @@ import com.example.clickerandroidarchitecture.R
 import java.util.*
 import android.graphics.drawable.RotateDrawable
 import android.provider.MediaStore
+import com.bumptech.glide.Glide
+import com.example.clickerandroidarchitecture.Gif
+import com.example.clickerandroidarchitecture.viewmodel.GifViewModel
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.giphy.sdk.core.models.Media
 import com.giphy.sdk.core.models.enums.RenditionType
@@ -21,48 +24,59 @@ import com.giphy.sdk.ui.GiphyCoreUI
 import com.giphy.sdk.ui.themes.GridType
 import com.giphy.sdk.ui.themes.LightTheme
 import com.giphy.sdk.ui.views.GPHMediaView
+import com.giphy.sdk.ui.views.GifView
 import com.giphy.sdk.ui.views.GiphyDialogFragment
 import kotlin.random.Random.Default.nextInt
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var countViewModel: CountViewModel
+    private lateinit var gifViewModel: GifViewModel
+
     private var count: Long = 0;
     private fun getUsername() = intent.extras?.get("username").toString().toLowerCase(Locale.US)
-    private var pics = arrayOf(R.mipmap.bacon, R.mipmap.french, R.mipmap.fluff, R.mipmap.boxer, R.mipmap.chihuaua, R.mipmap.husky)
+//    private var pics = arrayOf(R.mipmap.bacon, R.mipmap.french, R.mipmap.fluff, R.mipmap.boxer, R.mipmap.chihuaua, R.mipmap.husky)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val context = this
+        setContentView(R.layout.activity_main)
 
         //giphy
-        GiphyCoreUI.configure(this, "YdwxPNt3JQbgS8rKBDs3hawt4BhmmwHW")
-        var settings = GPHSettings(gridType = GridType.waterfall, theme = LightTheme, dimBackground = true)
-        val gifsDialog = GiphyDialogFragment.newInstance(settings)
-        settings.mediaTypeConfig = arrayOf(GPHContentType.gif)
-        gifsDialog.show(supportFragmentManager, "gifs_dialog")
+//        GiphyCoreUI.configure(this, "YdwxPNt3JQbgS8rKBDs3hawt4BhmmwHW")
+//        var settings = GPHSettings(gridType = GridType.waterfall, theme = LightTheme, dimBackground = true)
+//        val gifsDialog = GiphyDialogFragment.newInstance(settings)
+//        settings.mediaTypeConfig = arrayOf(GPHContentType.gif)
+//        gifsDialog.show(supportFragmentManager, "gifs_dialog")
 
 
-        gifsDialog.gifSelectionListener = object: GiphyDialogFragment.GifSelectionListener {
-            override fun onGifSelected(media: Media) {
+//        gifsDialog.gifSelectionListener = object: GiphyDialogFragment.GifSelectionListener {
+//            override fun onGifSelected(media: Media) {
+//
+//                image.setMedia(media, RenditionType.original)
+//            }
+//
+//            override fun onDismissed() {
+//                //Your user dismissed the dialog without selecting a GIF
+//            }
+//        }
 
-                image.setMedia(media, RenditionType.original)
-            }
 
-            override fun onDismissed() {
-                //Your user dismissed the dialog without selecting a GIF
-            }
-        }
-
-        //set only after gif has been selected
-        setContentView(R.layout.activity_main)
 
         welcomeUser.text = (welcomeUser.text).toString() + getUsername()
 
+        //count view model
         countViewModel = ViewModelProviders.of(this).get(CountViewModel::class.java)
         countViewModel.getUserCount(getUsername()).observe(this,
             androidx.lifecycle.Observer { updateCounter(it) })
+
+        //gif view model
+        gifViewModel = ViewModelProviders.of(this).get(GifViewModel::class.java)
+        gifViewModel.getRandomGif("android").observe(this,
+            androidx.lifecycle.Observer { loadGif(it) })
+
+
 
         myButton.setOnClickListener {
             countViewModel.setUserCount(getUsername(), count + 1)
@@ -78,6 +92,13 @@ class MainActivity : AppCompatActivity() {
     {
         count = c;
         counter.text = count.toString()
+    }
+
+    private fun loadGif(gif: Gif){
+        Glide.with(this)
+            .load(gif.url)
+            .into(image)
+
     }
 }
 
---
+++ Desktop/ChapmanUniversity/Fall2019/Android/AndroidClicker/app/src/main/java/com/example/clickerandroidarchitecture/viewmodel/GifViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.clickerandroidarchitecture.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.clickerandroidarchitecture.GifRepository
+
+class GifViewModel(application: Application) : AndroidViewModel(application){
+    private val repository = GifRepository(application.applicationContext)
+
+
+    fun getRandomGif(tag: String) = repository.getRandomGif(tag)
+
+
+}
